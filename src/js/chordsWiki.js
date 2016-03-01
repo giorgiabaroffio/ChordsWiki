73ad43e4d1ccc135fea0bad98d03d5b8121c6df5
giorgiabaroffio <giorgia.baroffio@gmail.com>
2016-03-01 12:15:14 +0100

    Jshint configuration

--- src/js/chordsWiki.js
+++ src/js/chordsWiki.js
@@ -1,25 +1,26 @@
-if (typeof(jQuery) === "undefined") {
-	throw("Unable to find jQuery");
+if (typeof(jQuery) === 'undefined') {
+	throw('Unable to find jQuery');
 }
 
-if (typeof(chordsWiki) === "undefined") {
+if (typeof(chordsWiki) === 'undefined') {
 	var chordsWiki = {};
 }
 
 (function() {
-	"use strict";
+	'use strict';
 
 	chordsWiki.Main = function() {
 
 		var self = this;
+
 		this.container = $('<div>');
+
 		var init = function() {
 			self.container.text('I am a chordWiki');
 			$('body').append(self.container);
 		};
 		init();
 
-	}
-
+	};
 
 }());
