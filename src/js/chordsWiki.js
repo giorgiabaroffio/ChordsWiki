1fdafd8c96866b07e17485cb3e9621a6a854a2e8
giorgiabaroffio <giorgia.baroffio@gmail.com>
2016-03-01 12:02:50 +0100

    First DOM append (div container)

--- src/js/chordsWiki.js
+++ src/js/chordsWiki.js
@@ -1,23 +1,25 @@
-if(typeof(jQuery) === "undefined"){
+if (typeof(jQuery) === "undefined") {
 	throw("Unable to find jQuery");
 }
 
-if(typeof(chordsWiki) === "undefined"){
+if (typeof(chordsWiki) === "undefined") {
 	var chordsWiki = {};
 }
 
-(function(){
+(function() {
 	"use strict";
 
-	chordsWiki.Main = function(){
+	chordsWiki.Main = function() {
 
+		var self = this;
+		this.container = $('<div>');
 		var init = function() {
-			console.log('test');
+			self.container.text('I am a chordWiki');
+			$('body').append(self.container);
 		};
 		init();
 
 	}
-
 
 
 }());
