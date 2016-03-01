ac98d2f371ea076fee2f3d5966d79ba974fc7049
giorgiabaroffio <giorgia.baroffio@gmail.com>
2016-03-01 13:36:14 +0100

    render() method added (DOM populated with main elements and selections)

--- src/js/chordsWiki.js
+++ src/js/chordsWiki.js
@@ -15,10 +15,43 @@ if (typeof(chordsWiki) === 'undefined') {
 
 		this.container = $('<div>');
 
+		var chordSelect = $('<select>');
+
+		var categorySelect = $('<select>');
+
 		var init = function() {
-			self.container.text('I am a chordWiki');
+			render();
 			$('body').append(self.container);
 		};
+
+		var render = function() {
+
+			//East area creation
+			var eastContainer = $('<div>');
+			eastContainer.addClass('east_area');
+			var subtitleEast = $('<h2>');
+			subtitleEast.text('Chord selection');
+			eastContainer.append(subtitleEast);
+			eastContainer.append(chordSelect);
+			eastContainer.append(categorySelect);
+
+			//West area creation
+			var westContainer = $('<div>');
+			westContainer.addClass('west_area');
+			var subtitleWest = $('<h2>');
+			subtitleWest.text('Chord details');
+			westContainer.append(subtitleWest);
+
+			//Main content wrapper creation
+			var mainContent = $('<main>');
+			mainContent.append(eastContainer);
+			mainContent.append(westContainer);
+
+			//Append to container
+			self.container.append(mainContent);
+
+		};
+
 		init();
 
 	};
