15080a1644abe00a4a2877a12708d7d65a0b368c
giorgiabaroffio <giorgia.baroffio@gmail.com>
2016-04-04 18:10:23 +0200

    test coverage for chordsWiki fixed

--- src/js/chordsWiki.js
+++ src/js/chordsWiki.js
@@ -1,8 +1,8 @@
-/* jshint camelcase: false*/
+/* istanbul ignore if */
 if (typeof(jQuery) === 'undefined') {
 	throw('Unable to find jQuery');
 }
-
+/* istanbul ignore else */
 if (typeof(chordsWiki) === 'undefined') {
 	/* jshint ignore:start */
 	var chordsWiki = {};
